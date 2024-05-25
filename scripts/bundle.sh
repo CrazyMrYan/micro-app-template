14d3ea88c6cc1709c2f4f3b3dacca717bbfcc316
严家辉 <crazymryan@gmail.com>
2024-05-25 17:16:48 +0800

    chore: bundle file change

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 OUTPUT_DIR=dist
+MIRCO_APPS_DIR=mirco-apps
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 
 rm -rf $OUTPUT_DIR
 
 mkdir $OUTPUT_DIR
-mkdir ./$OUTPUT_DIR/mirco-app
+mkdir ./$OUTPUT_DIR/$MIRCO_APPS_DIR
 
 # # sub-home子应用
 # cp -r ./sub-home/dist/ ./dist/subapp/sub-home/
 
 # sub-first子应用
-cp -r ./packages/field-system/dist/ $OUTPUT_DIR/mirco-app/field-system
+cp -r ./packages/field-system/dist/ $OUTPUT_DIR/$MIRCO_APPS_DIR/field-system
 rm -rf ./packages/field-system/dist/
 
 # main基座
@@ -19,5 +21,6 @@ rm -rf ./packages/main/dist/
 
 # cd ./dist
 # zip -r mp$(date +%Y%m%d%H%M%S).zip *
+tar -czvf dist-$TIMESTAMP.tar.gz $OUTPUT_DIR
 # cd ..
 echo 'bundle.sh execute success.'
